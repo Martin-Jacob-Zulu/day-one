c22953d03fefce37b7dea37cf63e588f318f5c72
Martin Jacob Zulu <jacob.zulu@cs.unza.zm>
2021-12-12 19:54:43 +0200

    Modified the file.py to add what we have looked at

--- file.py
+++ file.py
@@ -1,1 +1,23 @@
-print("Day one of python bootcamp")
+#Strings
+import math
+
+string = "We have done strings"
+#string[1] = "good" # This can't be done
+len(string) # This returns the number characters that a string has
+string[2] # This returns the character at index 2 of the string
+
+#Double
+# These are floating point numbers
+a = 20.5
+b = 15.3
+c = 20 / 7
+print(c)
+
+# Mathematical Operators
+1 + 1
+1 / 2
+2 * 2
+2 - 1
+2 % 3 # returns a remainder of the division
+2 ** 3 # This does the expontiation of 2
+
